7f77b8b37dbce613db4953bd287c606c4006ee20
Ash Kyd <kyd.ashley@abc.net.au>
2024-08-30 16:13:23 +1000

    feature: top bar style

--- src/components/Totals/index.tsx
+++ src/components/Totals/index.tsx
@@ -37,50 +37,51 @@ const Totals: React.FC<TotalsProps> = props => {
       data-consistent-incumbent={incumbent === previousIncumbent ? '' : undefined}
     >
       <div className={styles.text}>
-        {Object.keys(sides).map(allocation => (
+        {Object.keys(sides).map((allocation, i) => (
           <div key={allocation} className={styles.side} data-allocation={allocation}>
-            <span className={styles.label}>{sides[allocation]}</span>
-            <span className={styles.value}>{voteCounts[allocation]}</span>
+            <span className={styles.sideLabel}>{sides[allocation]}</span>&nbsp;
+            <span className={styles.sideValue}>{voteCounts[allocation]}</span>
           </div>
         ))}
       </div>
-      <div className={styles.track}>
-        <div
-          className={styles.bar}
-          title={`Likely Dem.: ${voteCounts[Allocation.LikelyDem]}`}
-          data-allocation={Allocation.LikelyDem}
-          style={{
-            transform: `translate(${tX(
-              voteCounts[Allocation.Dem] + voteCounts[Allocation.LikelyDem],
-              Allocation.Dem
-            )}%, 0)`
-          }}
-        ></div>
-        <div
-          className={styles.bar}
-          title={`Likely GOP: ${voteCounts[Allocation.LikelyGOP]}`}
-          data-allocation={Allocation.LikelyGOP}
-          style={{
-            transform: `translate(${tX(
-              voteCounts[Allocation.GOP] + voteCounts[Allocation.LikelyGOP],
-              Allocation.GOP
-            )}%, 0)`
-          }}
-        ></div>
-        <div
-          className={styles.bar}
-          title={`Dem.: ${voteCounts[Allocation.Dem]}`}
-          data-allocation={Allocation.Dem}
-          style={{ transform: `translate(${tX(voteCounts[Allocation.Dem], Allocation.Dem)}%, 0)` }}
-        ></div>
-        <div
-          className={styles.bar}
-          title={`GOP: ${voteCounts[Allocation.GOP]}`}
-          data-allocation={Allocation.GOP}
-          style={{ transform: `translate(${tX(voteCounts[Allocation.GOP], Allocation.GOP)}%, 0)` }}
-        ></div>
-        <div className={styles.midpoint}>
-          <div className={styles.midpointLabel}>{`${WIN_VOTES} to win`}</div>
+      <div className={styles.midpointLabel}>{`${WIN_VOTES} to win`}</div>
+      <div className={styles.trackRoot}>
+        <div className={styles.midpoint}></div>
+        <div className={styles.track}>
+          <div
+            className={styles.bar}
+            title={`Likely Dem.: ${voteCounts[Allocation.LikelyDem]}`}
+            data-allocation={Allocation.LikelyDem}
+            style={{
+              transform: `translate(${tX(
+                voteCounts[Allocation.Dem] + voteCounts[Allocation.LikelyDem],
+                Allocation.Dem
+              )}%, 0)`
+            }}
+          ></div>
+          <div
+            className={styles.bar}
+            title={`Likely GOP: ${voteCounts[Allocation.LikelyGOP]}`}
+            data-allocation={Allocation.LikelyGOP}
+            style={{
+              transform: `translate(${tX(
+                voteCounts[Allocation.GOP] + voteCounts[Allocation.LikelyGOP],
+                Allocation.GOP
+              )}%, 0)`
+            }}
+          ></div>
+          <div
+            className={styles.bar}
+            title={`Dem.: ${voteCounts[Allocation.Dem]}`}
+            data-allocation={Allocation.Dem}
+            style={{ transform: `translate(${tX(voteCounts[Allocation.Dem], Allocation.Dem)}%, 0)` }}
+          ></div>
+          <div
+            className={styles.bar}
+            title={`GOP: ${voteCounts[Allocation.GOP]}`}
+            data-allocation={Allocation.GOP}
+            style={{ transform: `translate(${tX(voteCounts[Allocation.GOP], Allocation.GOP)}%, 0)` }}
+          ></div>
         </div>
       </div>
     </div>
