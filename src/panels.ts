cf7eca3b9af22b3053dc5b6368277248ce9c6cea
Ash Kyd <kyd.ashley@abc.net.au>
2024-10-16 14:54:40 +1000

    feature: only highlight bolded states

--- src/panels.ts
+++ src/panels.ts
@@ -9,88 +9,36 @@ import TextState from './components/TextState/TextState';
 const SORTED_STATES = STATES.sort((a, b) => b.name.length - a.name.length);
 
 export function applyColourToPanels(panels: PanelDefinition<PossiblyEncodedGraphicProps>[]) {
-  const stateIntroductionTracker: { [key: string]: boolean } = {};
-
   panels.forEach(({ data, nodes }, i) => {
-    const stateIntroductionTrackerPanel: { [key: string]: boolean } = {};
     const hasFocuses = getHasFocuses(data.focuses);
-    const textNodes = nodes.reduce<Node[]>((memo, node) => memo.concat(textNodesUnder(node)), []);
 
-    textNodes.forEach(node => {
-      let text = node.textContent || '';
-
-      SORTED_STATES.forEach(({ name }) => {
-        const index = text.indexOf(name);
-        if (index > -1 && text[index - 1] !== '|' && text[index + name.length] !== '|') {
-          text = text.replace(name, `|||${name}|||`);
-        }
-      });
-
-      if (text === node.textContent) {
-        return;
-      }
-
-      const parentEl = node.parentElement;
-
-      if (!parentEl) {
-        return;
-      }
-
-      text.split('|||').forEach((part, index) => {
-        const partTextNode = document.createTextNode(part);
-
-        if (!(index % 2)) {
-          return parentEl.insertBefore(partTextNode, node);
-        }
-
-        const state = STATES.find(({ name }) => name === part);
+    (nodes as HTMLElement[]).forEach(node =>
+      node.querySelectorAll('strong').forEach(stronk => {
+        const state = STATES.find(({ name }) => name === stronk.textContent?.trim());
         const stateId = state ? StateId[state.id] : null;
-
-        // If this isn't a state, or we've seen this state before
-        if (!stateId || stateIntroductionTrackerPanel[stateId]) {
-          return parentEl.insertBefore(partTextNode, node);
+        if (!stateId) {
+          return;
         }
-        stateIntroductionTrackerPanel[stateId] = true;
+        const newParent = document.createElement('span');
+        stronk.replaceWith(newParent);
 
-        const partWrapperNode = document.createElement('span');
-        const { allocations, relative } = data as GraphicProps;
+        const { allocations } = data as GraphicProps;
         let stateMainAllocation = allocations && getStateAllocations(stateId, allocations)[0];
         if (stateMainAllocation === 'n' && hasFocuses) {
           stateMainAllocation = Allocation.UnallocatedFocused;
         }
-        const relativeAllocations = relative && PRESETS[relative]?.allocations;
-        const stateRelativeMainAllocation = relativeAllocations && getStateAllocations(stateId, relativeAllocations)[0];
 
-        parentEl.insertBefore(partWrapperNode, node);
-
-        const isFirstEncounter = !stateIntroductionTracker[stateId];
-        stateIntroductionTracker[stateId] = true;
         render(
           createElement(TextState, {
             name: state?.name,
             id: stateId,
             allocation: stateMainAllocation,
-            showAbbr: isFirstEncounter
+            showAbbr: true
           }),
-          partWrapperNode
+          newParent
         );
-
-        // if (stateMainAllocation) {
-        //   partWrapperNode.setAttribute('data-main-allocation', stateMainAllocation);
-        // }
-
-        // if (stateRelativeMainAllocation) {
-        //   partWrapperNode.setAttribute('data-relative-main-allocation', stateRelativeMainAllocation);
-        // }
-
-        // partWrapperNode.setAttribute('data-state', stateId);
-        // partWrapperNode.className = blockStyles.state;
-        // partWrapperNode.appendChild(partTextNode);
-        // parentEl.insertBefore(partWrapperNode, node);
-      });
-
-      parentEl.removeChild(node);
-    });
+      })
+    );
   });
 }
 
