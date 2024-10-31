832e6e95f21d5233f057de4cfaa841b028442c39
Ash Kyd <kyd.ashley@abc.net.au>
2024-10-31 14:48:39 +1000

    feature: allow the words "solid" and "likely" to be highlighted

--- src/panels.ts
+++ src/panels.ts
@@ -6,19 +6,54 @@ import type { GraphicProps, PossiblyEncodedGraphicProps } from './components/Gra
 import { render } from 'react-dom';
 import TextState from './components/TextState/TextState';
 
+function hasWords(words, text) {
+  return words.some(word => text.includes(word));
+}
+
+function replaceNodeWithTextState(node, props) {
+  const newParent = document.createElement('span');
+  node.replaceWith(newParent);
+  render(createElement(TextState, props), newParent);
+}
+
 export function applyColourToPanels(panels: PanelDefinition<PossiblyEncodedGraphicProps>[]) {
   panels.forEach(({ data, nodes }, i) => {
     const hasFocuses = getHasFocuses(data.focuses);
 
     (nodes as HTMLElement[]).forEach(node =>
       node.querySelectorAll('strong').forEach(stronk => {
+        const textContent = String(stronk.textContent).trim();
+        const magicWords = ['solid', 'likely'];
+
+        if (magicWords.includes(textContent)) {
+          const redWords = ['trump', 'donald', 'republican'];
+          const blueWords = ['harris', 'kamala', 'democrat'];
+          const blockText = String(node.textContent).toLowerCase();
+          const isRed = hasWords(redWords, blockText);
+          const isBlue = hasWords(blueWords, blockText);
+
+          let allocation = 'n';
+          if (isBlue) {
+            allocation = textContent === 'solid' ? 'd' : 's';
+          }
+          if (isRed) {
+            allocation = textContent === 'solid' ? 'r' : 'e';
+          }
+
+          replaceNodeWithTextState(stronk, {
+            name: textContent,
+            allocation: allocation,
+            showAbbr: false
+          });
+
+          return;
+        }
+
         const state = STATES.find(({ name }) => name === stronk.textContent?.trim());
         const stateId = state ? StateId[state.id] : null;
         if (!stateId) {
           return;
         }
-        const newParent = document.createElement('span');
-        stronk.replaceWith(newParent);
 
         const { allocations } = data as GraphicProps;
         let stateMainAllocation = allocations && getStateAllocations(stateId, allocations)[0];
@@ -26,15 +61,12 @@ export function applyColourToPanels(panels: PanelDefinition<PossiblyEncodedGraph
           stateMainAllocation = Allocation.UnallocatedFocused;
         }
 
-        render(
-          createElement(TextState, {
-            name: state?.name,
-            id: stateId,
-            allocation: stateMainAllocation,
-            showAbbr: true
-          }),
-          newParent
-        );
+        replaceNodeWithTextState(stronk, {
+          name: state?.name,
+          id: stateId,
+          allocation: stateMainAllocation,
+          showAbbr: true
+        });
       })
     );
   });
